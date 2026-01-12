de0fb09410452991d22ca719f97adbd070fab4f2
izukamma <izukammagabriel@gmail.com>
2026-01-11 21:57:26 -0600

    Day 0 & Day 1 — project setup and first JS file

---
+++ day-1/index.js
@@ -0,0 +1,19 @@
+console.log("Hello, World!");
+console.log("my name is gabriel");
+console.log("I am learning JavaScript");
+
+let name = "gabriel";
+
+const firstName = "gabriel";
+let age = 25;
+
+console.log("first name:", firstName);
+console.log("age:", age);
+
+age = age + 1;
+console.log("next year:", age);
+
+typeof firstName;
+typeof age;
+
+// 1️⃣ Learn (20 minutes)

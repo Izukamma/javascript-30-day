fbde408f745a34d1c6cfdd55f76b948dc9dc5edd
Gabriel <Izukammagabriel@gmail.com>
2026-01-13 19:51:32 -0600

    day 5

--- day-1/index.js
+++ day-1/index.js
@@ -1,3 +1,5 @@
+// 1️⃣ Learn (20 minutes)
+
 console.log("Hello, World!");
 console.log("my name is gabriel");
 console.log("I am learning JavaScript");
@@ -13,8 +15,41 @@ console.log("age:", age);
 age = age + 1;
 console.log("next year:", age);
 
+// day 2
+
 typeof firstName;
 typeof age;
 
 console.log(`Hello, my name is ${firstName}`);
-// 1️⃣ Learn (20 minutes)
+
+//day 3
+
+let balance = 10000;
+let riskPercent = 1;
+
+let riskAmount = balance * (riskPercent / 100);
+console.log("risk amount:", riskAmount);
+
+console.log(balance > 5000);
+console.log(balance === 10000);
+
+let isValidTrade = balance > 5000 && riskPercent <= 2;
+console.log("is valid trade:", isValidTrade);
+
+let accountBalance = 1200;
+
+if (accountBalance > 1000) {
+  console.log("You can trade today");
+} else {
+  console.log("No trades today");
+}
+
+let score = 85;
+
+if (score >= 90) {
+  console.log("A");
+} else if (score >= 80) {
+  console.log("B");
+} else {
+  console.log("C");
+}
